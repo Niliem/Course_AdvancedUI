afe8d1916a432f7ac9103c0db74a9a174d445165
SolidPenguin <>
2025-08-03 16:40:56 +0300

    10. Widget Stack Gameplay Tags

---
+++ Source/FrontendUI/Private/FrontendGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FrontendGameplayTags.h"
+
+namespace FrontendGameplayTags
+{
+    UE_DEFINE_GAMEPLAY_TAG(Frontend_Layer_Modal, "Frontend.Layer.Modal");
+    UE_DEFINE_GAMEPLAY_TAG(Frontend_Layer_GameMenu, "Frontend.Layer.GameMenu");
+    UE_DEFINE_GAMEPLAY_TAG(Frontend_Layer_GameHUD, "Frontend.Layer.GameHUD");
+    UE_DEFINE_GAMEPLAY_TAG(Frontend_Layer_Frontend, "Frontend.Layer.Frontend");
+}
---
+++ Source/FrontendUI/Public/FrontendGameplayTags.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace FrontendGameplayTags
+{
+    FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Layer_Modal);
+    FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Layer_GameMenu);
+    FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Layer_GameHUD);
+    FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Layer_Frontend);
+}; //FrontendGameplayTags
