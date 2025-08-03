872e76cada77b1eccb7633c82e97e71baa78abc4
SolidPenguin <>
2025-08-03 22:48:51 +0300

    23. Frontend Function Library

---
+++ Source/FrontendUI/Private/FrontendFunctionLibrary.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FrontendFunctionLibrary.h"
+#include "Settings/FrontendDeveloperSettings.h"
+
+TSoftClassPtr<UWidget_ActivatableBase> UFrontendFunctionLibrary::GetFrontendWidgetClassByTag(FGameplayTag WidgetTag)
+{
+    const UFrontendDeveloperSettings* FrontendDeveloperSettings = GetDefault<UFrontendDeveloperSettings>();
+
+    checkf(FrontendDeveloperSettings->FrontendWidgetMap.Contains(WidgetTag), TEXT("Could not find the corresponding widget under the tag %s"), *WidgetTag.ToString());
+
+    return FrontendDeveloperSettings->FrontendWidgetMap.FindRef(WidgetTag);
+}
