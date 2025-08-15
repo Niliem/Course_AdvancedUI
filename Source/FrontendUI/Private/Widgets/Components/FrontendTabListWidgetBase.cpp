333707e7fd87b26d22a68385d3e8c1dcbb655026
SolidPenguin <>
2025-08-15 14:35:48 +0300

    55. Common Tab List Widget

---
+++ Source/FrontendUI/Private/Widgets/Components/FrontendTabListWidgetBase.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Components/FrontendTabListWidgetBase.h"
+#include "Editor/WidgetCompilerLog.h"
+#include "Widgets/Components/FrontendCommonButtonBase.h"
+
+#if WITH_EDITOR
+void UFrontendTabListWidgetBase::ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const
+{
+    Super::ValidateCompiledDefaults(CompileLog);
+
+    if (!TabButtonEntryWidgetClass)
+    {
+        CompileLog.Error(FText::FromString(TEXT("The variable TabButtonEntryWidgetClass has no valid entry specified. ")
+            + GetClass()->GetName()
+            + " needs a valid entry widget class to functional properly"));    
+    }
+}
+#endif
