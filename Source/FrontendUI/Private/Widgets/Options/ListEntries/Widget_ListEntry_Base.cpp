b2e4938e99705c9250bcc627ebcda8197c6b700d
SolidPenguin <>
2025-08-24 15:54:55 +0300

    68. List Entry Widget Base

---
+++ Source/FrontendUI/Private/Widgets/Options/ListEntries/Widget_ListEntry_Base.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Options/ListEntries/Widget_ListEntry_Base.h"
+
+void UWidget_ListEntry_Base::NativeOnListItemObjectSet(UObject* ListItemObject)
+{
+    IUserObjectListEntry::NativeOnListItemObjectSet(ListItemObject);
+}
