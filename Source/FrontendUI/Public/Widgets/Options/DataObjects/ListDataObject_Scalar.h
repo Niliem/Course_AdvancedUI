445fba923f8e7e11953224ffae889af774910120
SolidPenguin <>
2025-09-03 17:48:06 +0300

    114. Scalar Data Object

---
+++ Source/FrontendUI/Public/Widgets/Options/DataObjects/ListDataObject_Scalar.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Options/DataObjects/ListDataObject_Value.h"
+#include "CommonNumericTextBlock.h"
+#include "ListDataObject_Scalar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDUI_API UListDataObject_Scalar : public UListDataObject_Value
+{
+	GENERATED_BODY()
+
+public:
+    LIST_DATA_ACCESSOR(TRange<float>, DisplayValueRange);
+    LIST_DATA_ACCESSOR(TRange<float>, OutputValueRange);
+    LIST_DATA_ACCESSOR(float, SliderStepSize);
+    LIST_DATA_ACCESSOR(ECommonNumericType, DisplayNumericType);
+    LIST_DATA_ACCESSOR(FCommonNumberFormattingOptions, NumberFormattingOptions);    
+
+private:
+    TRange<float> DisplayValueRange = TRange<float>(0.0f, 1.0f);
+    TRange<float> OutputValueRange = TRange<float>(0.0f, 1.0f);
+    float SliderStepSize = 0.1f;
+
+    ECommonNumericType DisplayNumericType = ECommonNumericType::Number;
+    FCommonNumberFormattingOptions NumberFormattingOptions;    
+};
